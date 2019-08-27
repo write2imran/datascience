7f2271da0f33b96ca1fe813a9e28ff03f20a8092
Imran Rauf <write2imran@yahoo.com>
2019-08-27 22:27:11 +0300

    init

---
+++ sl/perceptron/main.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 27 22:15:31 2019
+
+@author: IMRANAX
+"""
+from plotIris import PlotIrisData
+
+PlotIrisData()
---
+++ sl/perceptron/plotIris.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 27 22:08:03 2019
+
+@author: IMRANAX
+"""
+
+import pandas as pd
+#%matplotlib inline
+import matplotlib.pyplot as plt
+import numpy as np
+
+def PlotIrisData() 
+    df = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data', header=None)
+    
+    
+    # select setosa and versicolor
+    y = df.iloc[0:100, 4].values
+    y = np.where(y == 'Iris-setosa', -1, 1)
+    
+    # extract sepal length and petal length
+    X = df.iloc[0:100, [0, 2]].values
+    
+    # plot data
+    plt.scatter(X[:50, 0], X[:50, 1],
+                color='red', marker='o', label='setosa')
+    plt.scatter(X[50:100, 0], X[50:100, 1],
+                color='blue', marker='x', label='versicolor')
+    
+    plt.xlabel('sepal length [cm]')
+    plt.ylabel('petal length [cm]')
+    plt.legend(loc='upper left')
+    
+    # plt.savefig('images/02_06.png', dpi=300)
+    plt.show()
